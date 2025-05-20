f97c77666e2527c6b8884c37cf30539319a17f6f
biog4m3 <biog4m3ryt@gmail.com>
2025-05-20 11:00:38 -0300

    feat: initialize backend with Fastify and Prisma setup
    - Added package.json and package-lock.json for project dependencies.
    - Created Prisma schema for Doorman, Vehicle, and Movements models.
    - Implemented Fastify server with endpoints for vehicle movements.
    - Added service file for doorman operations (currently empty).

---
+++ backend/src/app.js
@@ -0,0 +1,34 @@
+const fastify = require('fastify');
+const { PrismaClient } = require('@prisma/client');
+
+const fastifyInstance = fastify({ logger: true });
+const prisma = new PrismaClient();
+
+fastifyInstance.get('/api/vehicles/out', async (request, reply) => {
+  const resultado = await prisma.$queryRawUnsafe(`
+    SELECT v.id, v.plate, v.model, v.brand, v.driverName, m.date AS exitDate
+    FROM Vehicle v
+    JOIN (
+    SELECT vehicleId, MAX(date) AS lastMovementDate
+    FROM Movements
+    GROUP BY vehicleId
+    ) AS lastMovements ON v.id = lastMovements.vehicleId
+    JOIN Movements m ON m.vehicleId = v.id AND m.date = lastMovements.lastMovementDate
+    WHERE m.type = 'exit';
+  `);
+  return resultado;
+});
+
+fastifyInstance.get('/api/vehicles/movements', async (request, reply) => {
+  //all vehicles movements
+  const resultado = await prisma.$queryRawUnsafe(`
+    SELECT v.id, v.plate, v.model, v.brand, v.driverName, m.date AS movementDate, m.type AS movementType
+    FROM Vehicle v
+    JOIN Movements m ON m.vehicleId = v.id
+    ORDER BY m.date DESC;
+  `);
+
+  return resultado;
+});
+
+module.exports = fastifyInstance;
---
+++ backend/src/index.js
@@ -0,0 +1,10 @@
+const app = require('./app');
+
+const PORT = Number(process.env.PORT) || 4000;
+app.listen({ port: PORT, host: "0.0.0.0" }, (err, address) => {
+    if (err) {
+        app.log.error(err);
+        process.exit(1);
+    }
+    app.log.info(`Server is running at ${address}`);
+})
